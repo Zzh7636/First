0fc92218046f174eb2e871551c5eefbc6796d9fa
zhouziheng <136667636@qq.com>
2019-06-27 22:01:16 +0800

    1.项目基本结构创建完成 2.登录功能基本完成

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 引入element UI 库
+import ElementUI from 'element-ui';
+//  引入css文件
+import 'element-ui/lib/theme-chalk/index.css';
+
+import './assets/css/index.css';
+
 Vue.config.productionTip = false
+
+Vue.use(ElementUI);
 
 new Vue({
   router,
